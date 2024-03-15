325639cc708e517e31e89afb3de0904a741109ac
YousefSaeed668 <yousefsaeed668@gmail.com>
2024-03-16 00:09:42 +0200

    create collectionform and complete it's functionality

---
+++ app/(dashboard)/collections/new/page.tsx
@@ -0,0 +1,7 @@
+import CollectionsForm from "@/components/collections/CollectionsForm";
+
+const CreateCollection = () => {
+  return <CollectionsForm />;
+};
+
+export default CreateCollection;
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+import TopBar from "@/components/layout/TopBar";
+import { ToastProvider } from "@/lib/ToastProvider";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chicly - Admin Dashboard",
+  description: "Admin Dashboard to Manage Chicly Data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          <div className="flex max-lg:flex-col text-grey-1">
+            <LeftSideBar />
+            <TopBar />
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
